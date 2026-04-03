e9c84f2c098446d14fd7735b54cc46c76d40f661
cjxu <123421156@qq.com>
2026-04-03 09:48:22 +0800

    test: add encoded issue type classification helper

---
+++ include/gpu_model/execution/internal/encoded_issue_type.h
@@ -0,0 +1,36 @@
+#pragma once
+
+#include "gpu_model/execution/internal/issue_model.h"
+#include "gpu_model/instruction/encoded/internal/encoded_instruction_descriptor.h"
+
+namespace gpu_model {
+
+inline ArchitecturalIssueType ArchitecturalIssueTypeForEncodedInstruction(
+    const DecodedInstruction& instruction,
+    const EncodedInstructionDescriptor& descriptor) {
+  if (instruction.mnemonic == "s_barrier") {
+    return ArchitecturalIssueType::Special;
+  }
+  if (instruction.mnemonic.starts_with("s_cbranch")) {
+    return ArchitecturalIssueType::Branch;
+  }
+
+  switch (descriptor.category) {
+    case EncodedInstructionCategory::ScalarMemory:
+    case EncodedInstructionCategory::Scalar:
+      return ArchitecturalIssueType::ScalarAluOrMemory;
+    case EncodedInstructionCategory::Memory:
+      return instruction.mnemonic.starts_with("ds_")
+                 ? ArchitecturalIssueType::LocalDataShare
+                 : ArchitecturalIssueType::VectorMemory;
+    case EncodedInstructionCategory::Vector:
+      return instruction.mnemonic.starts_with("ds_")
+                 ? ArchitecturalIssueType::LocalDataShare
+                 : ArchitecturalIssueType::VectorAlu;
+    case EncodedInstructionCategory::Unknown:
+    default:
+      return ArchitecturalIssueType::Special;
+  }
+}
+
+}  // namespace gpu_model
